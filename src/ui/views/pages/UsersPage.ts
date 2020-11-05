100d5809fdc94d4ae0106c2c49cc903281dd32aa
Aleksandr Kondrev <a.kondrev@gmail.com>
2020-11-05 13:43:35 +0300

    Update users page

--- src/ui/views/pages/UsersPage.ts
+++ src/ui/views/pages/UsersPage.ts
@@ -1,6 +1,75 @@
 import { Component, Vue } from 'vue-property-decorator'
+import SearchField from '@/ui/components/SearchField.vue'
+import FiltersContainer from '@/ui/components/FiltersContainer.vue'
+import DataTable from '@/ui/components/DataTable.vue'
+import { TableHeader } from '@/models/TableHeader'
+import { TableOptions } from '@/models/TableOptions'
 
-@Component
+@Component({
+  components: {
+    SearchField,
+    FiltersContainer,
+    DataTable
+  }
+})
 export default class UsersPage extends Vue {
+  /// --- TABLE ---
+  protected readonly tableHeaders: Array<TableHeader> = [
+    {
+      value: 'id',
+      align: 'start',
+      sortable: false,
+      divider: false,
+      text: this.$vuetify.lang.t('$vuetify.pages.users.table.headers.0'),
+      width: '6%'
+    },
+    {
+      value: 'name',
+      align: 'start',
+      sortable: false,
+      divider: false,
+      text: this.$vuetify.lang.t('$vuetify.pages.users.table.headers.1'),
+      width: '22%'
+    },
+    {
+      value: 'instance',
+      align: 'start',
+      sortable: false,
+      divider: false,
+      text: this.$vuetify.lang.t('$vuetify.pages.users.table.headers.2'),
+      width: '15%'
+    },
+    {
+      value: 'role',
+      align: 'start',
+      sortable: false,
+      divider: false,
+      text: this.$vuetify.lang.t('$vuetify.pages.users.table.headers.3'),
+      width: '15%'
+    },
+    {
+      value: 'status',
+      align: 'start',
+      sortable: false,
+      divider: false,
+      text: this.$vuetify.lang.t('$vuetify.pages.users.table.headers.4'),
+      width: '15%'
+    },
+    {
+      value: 'actions',
+      align: 'start',
+      sortable: false,
+      divider: false,
+      text: this.$vuetify.lang.t('$vuetify.pages.users.table.headers.5'),
+      width: '18%'
+    }
+  ]
 
+  protected tableLoading = false
+  protected tableOptions!: TableOptions
+  /// --- END TABLE ---
+  /// --- FILTERS ---
+  protected filterActive = 1
+  protected filterQuery = ''
+  /// --- END FILTERS ---
 }
--- src/ui/views/pages/UsersPage.vue
+++ src/ui/views/pages/UsersPage.vue
@@ -1,5 +1,73 @@
 <template>
-  <div>Users</div>
+  <v-container fluid>
+    <DataTable
+      ref="table"
+      :headers="tableHeaders"
+      :items="tableItems"
+      :server-items-length="totalItems"
+      :search="filterQuery"
+      :loading="tableLoading"
+      @options="onOptionsChanged"
+    >
+      <template slot="top">
+        <FiltersContainer class="px-4">
+          <SearchField
+            v-model="filterQuery"
+            max-width="450px"
+            :placeholder="$vuetify.lang.t('$vuetify.common.table.search_input')"
+          />
+
+          <v-spacer class="mx-2" />
+
+          <v-btn
+            :text="$vuetify.breakpoint.mdAndUp"
+            :icon="!$vuetify.breakpoint.mdAndUp"
+            :disabled="!isClearFiltersButtonEnable"
+            :title="$vuetify.lang.t(`$vuetify.common.actions.clear_filter`)"
+            @click="clearFilters"
+          >
+            <span v-if="$vuetify.breakpoint.mdAndUp">{{ $vuetify.lang.t(`$vuetify.common.actions.clear_filter`) }}</span>
+            <v-icon v-else>mdi-filter-remove-outline</v-icon>
+          </v-btn>
+        </FiltersContainer>
+
+        <v-divider class="mt-3" />
+      </template>
+
+      <template
+        slot="item"
+        slot-scope="{ item }"
+      >
+        <tr class="cursor-pointer">
+          <td class="text-start user-select-none">
+            <span>{{ item.id }}</span>
+          </td>
+          <td class="text-start user-select-none">
+            <span>{{ item.name }}</span>
+          </td>
+          <td class="text-start user-select-none">
+            <span>{{ item.limit }}</span>
+          </td>
+          <td class="text-start user-select-none">
+            <span>{{ item.count }}</span>
+          </td>
+          <td class="text-start user-select-none">
+            <v-chip color="grey lighten-4">{{ item.created_at }}</v-chip>
+          </td>
+          <td class="text-start user-select-none">
+            <v-chip color="grey lighten-4">{{ item.expires_at }}</v-chip>
+          </td>
+          <td class="text-center user-select-none">
+            <v-chip
+              v-if="item.state"
+              color="error lighten-1"
+            >{{ $vuetify.lang.t('$vuetify.pages.instances.table.need_update_lic') }}</v-chip>
+            <span v-else>—</span>
+          </td>
+        </tr>
+      </template>
+    </DataTable>
+  </v-container>
 </template>
 
 <script src="./UsersPage" lang="ts" />
