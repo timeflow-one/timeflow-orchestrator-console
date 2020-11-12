955ead4b533f5619e5837b7ee7e097479ad11f10
Aleksandr Kondrev <a.kondrev@gmail.com>
2020-11-12 22:56:06 +0300

    add table headers for licenses page

--- src/ui/views/pages/LicensesPage.ts
+++ src/ui/views/pages/LicensesPage.ts
@@ -1,6 +1,62 @@
+import { TableHeader } from '@/models/TableHeader'
+import { TableOptions } from '@/models/TableOptions'
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
 export default class LicensesPage extends Vue {
+  /// --- TABLE ---
+  protected readonly tableHeaders: Array<TableHeader> = [
+    {
+      value: 'id',
+      align: 'start',
+      sortable: false,
+      divider: false,
+      text: this.$vuetify.lang.t('$vuetify.pages.plans.table.header.0'),
+      width: '6%'
+    },
+    {
+      value: 'name',
+      align: 'start',
+      sortable: false,
+      divider: false,
+      text: this.$vuetify.lang.t('$vuetify.pages.plans.table.header.1'),
+      width: '22%'
+    },
+    {
+      value: 'code',
+      align: 'start',
+      sortable: false,
+      divider: false,
+      text: this.$vuetify.lang.t('$vuetify.pages.plans.table.header.2'),
+      width: '15%'
+    },
+    {
+      value: 'limit',
+      align: 'start',
+      sortable: false,
+      divider: false,
+      text: this.$vuetify.lang.t('$vuetify.pages.plans.table.header.3'),
+      width: '15%'
+    },
+    {
+      value: 'price',
+      align: 'start',
+      sortable: false,
+      divider: false,
+      text: this.$vuetify.lang.t('$vuetify.pages.plans.table.header.4'),
+      width: '15%'
+    },
+    {
+      value: 'status',
+      align: 'center',
+      sortable: false,
+      divider: false,
+      text: this.$vuetify.lang.t('$vuetify.pages.plans.table.header.5'),
+      width: '18%'
+    }
+  ]
 
+  protected tableLoading = false
+  protected tableOptions!: TableOptions
+  /// --- END TABLE ---
 }
